be70187a9a9cafcdf6166bec0099bb5cd7265c3e
Ojitha Kumanayaka <ojithak@ticketek.com.au>
2017-11-17 16:35:32 +1100

    Added the sort example.

--- sort_on_priority.py
+++ sort_on_priority.py
@@ -13,3 +13,16 @@ print(num)
 b = [(1,8),(0,3),(1,1),(0,2),(0,5),(1,4),(0,7),(1,6)]
 b.sort()
 b
+
+#using class
+class SortHelper (object):
+    def __init__(self, group):
+        self.group = group
+
+    def __call__(self, x):
+        if x in self.group:
+            return (0,x)
+        return (1,x)
+
+helper =  SortHelper(group)
+num.sort(key=helper)
